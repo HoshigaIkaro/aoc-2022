6ed32d8ccc490c76fee9441ccb1e6d1d5c753f6a
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-03 23:22:27 -0600

    change to declarative format

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -4,35 +4,32 @@ pub struct Day4;
 
 impl Day for Day4 {
     fn part_1(&self, input: &str) -> String {
-        let mut total = 0;
-        for line in input.lines() {
-            let (one, two) = line.split_once(',').unwrap();
-            let (l_1, h_1) = get_range(one);
-            let (l_2, h_2) = get_range(two);
-            if (l_1 >= l_2 && h_1 <= h_2) || (l_2 >= l_1 && h_2 <= h_1) { 
-                total += 1;
-            }
-        }
-        total.to_string()
+        input
+            .lines()
+            .map(|line| {
+                line.split(',')
+                    .flat_map(|range| range.split('-'))
+                    .map(|s| s.parse::<u8>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .filter(|arr| {
+                (arr[0] >= arr[2] && arr[1] <= arr[3]) || (arr[2] >= arr[0] && arr[3] <= arr[1])
+            })
+            .count()
+            .to_string()
     }
 
     fn part_2(&self, input: &str) -> String {
-        let mut total = 0;
-        for line in input.lines() {
-            let (one, two) = line.split_once(',').unwrap();
-            let (l_1, h_1) = get_range(one);
-            let (l_2, h_2) = get_range(two);
-            if (l_1 >= l_2 && l_1 <= h_2)
-            || (l_2 >= l_1 && l_2 <= h_1)
-             { 
-                total += 1;
-            }
-        }
-        total.to_string()
+        input
+            .lines()
+            .map(|line| {
+                line.split(',')
+                    .flat_map(|range| range.split('-'))
+                    .map(|s| s.parse::<u8>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .filter(|arr| arr[0] <= arr[3] && arr[2] <= arr[1])
+            .count()
+            .to_string()
     }
-}
-
-fn get_range(s: &str) -> (u8, u8) {
-    let (a, b) = s.split_once('-').unwrap();
-    (a.parse().unwrap(), b.parse().unwrap())
 }
