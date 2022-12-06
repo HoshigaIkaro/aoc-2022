4dc07184a335bb7e552c5e6c3104ea3bf48017f5
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-05 23:27:55 -0600

    optimized day 6

--- src/days/day_06.rs
+++ src/days/day_06.rs
@@ -1,53 +1,43 @@
+use std::ops::Add;
+
 use super::Day;
 
 pub struct Day06;
 
 impl Day for Day06 {
     fn part_1(&self, input: &str) -> String {
-        {
-            input
-                .chars()
-                .collect::<Vec<char>>()
-                .windows(4)
-                .enumerate()
-                .find(|(i, window)| {
-                    let mut s = Vec::with_capacity(4);
-                    for c in window.iter() {
-                        if s.contains(c) {
-                            return false;
-                        }
-                        s.push(*c);
-                    }
-                    true
-                })
-                .unwrap()
-                .0
-                + 4
-        }
-        .to_string()
+        input
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(4)
+            .enumerate()
+            .find(|(_i, window)| {
+                window
+                    .iter()
+                    .enumerate()
+                    .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
+            })
+            .unwrap()
+            .0
+            .add(4)
+            .to_string()
     }
 
     fn part_2(&self, input: &str) -> String {
-        {
-            input
-                .chars()
-                .collect::<Vec<char>>()
-                .windows(14)
-                .enumerate()
-                .find(|(i, window)| {
-                    let mut s = Vec::with_capacity(14);
-                    for c in window.iter() {
-                        if s.contains(c) {
-                            return false;
-                        }
-                        s.push(*c);
-                    }
-                    true
-                })
-                .unwrap()
-                .0
-                + 14
-        }
-        .to_string()
+        input
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(14)
+            .enumerate()
+            .find(|(_i, window)| {
+                window
+                    .iter()
+                    .enumerate()
+                    .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
+            })
+            .unwrap()
+            .0
+            .add(14)
+            .to_string()
     }
 }
