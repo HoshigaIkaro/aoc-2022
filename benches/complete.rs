666a49a46fa8de13c0c8bff92ad3d1bd1ed3dae9
HoshigaIkaro <hoshiga.ikaro@outlook.com>
2022-12-07 09:51:28 -0600

    solved day 7

--- benches/complete.rs
+++ benches/complete.rs
@@ -40,6 +40,7 @@ bench_day!(03);
 bench_day!(04);
 bench_day!(05);
 bench_day!(06);
+bench_day!(07);
 
 criterion_group!(complete, day_01, day_02, day_03, day_04, day_05, day_06);
 criterion_main!(complete);
--- src/days/day_07.rs
+++ src/days/day_07.rs
@@ -1,61 +1,106 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::iter;
 
 use super::Day;
+
+type Dirs = HashMap<u64, Vec<u64>>;
+type Files = HashMap<u64, Vec<u64>>;
 
 pub struct Day07;
 
 impl Day for Day07 {
     fn part_1(&self, input: &str) -> String {
-        let mut dirs: HashMap<&str, Vec<&str>> = HashMap::new();
-        let mut files: HashMap<&str, Vec<usize>> = HashMap::new();
-        let mut dir_stack = vec!["/"];
-
-        for line in input.lines() {
-            if line.starts_with("$ l") {
-                continue;
-            } else if line.starts_with("$ c") {
-                match line.trim_start_matches("$ cd ") {
-                    ".." => {
-                        dir_stack.pop();
-                    }
-                    "/" => dir_stack.truncate(1),
-                    dir => dir_stack.push(dir),
-                }
-            } else if line.starts_with("d") {
-                let dir = line.trim_start_matches("dir ");
-                dirs.entry(dir_stack.last().unwrap()).or_default().push(dir);
-                dirs.insert(dir, Vec::new());
-            } else {
-                files
-                    .entry(dir_stack.last().unwrap())
-                    .or_default()
-                    .push(line.split_whitespace().next().unwrap().parse().unwrap())
+        let (_root, dirs, files) = parse(input);
+        let mut sum = 0;
+        for con in dirs.keys() {
+            let size = size(*con, &dirs, &files);
+            if size <= 100_000 {
+                sum += size;
             }
         }
 
-        let mut total = 0;
-        for dir in dirs.keys() {
-            let sum = size(dir, &dirs, &files);
-
-            if sum <= 100000 {
-                total += sum;
-            }
-        }
-
-        total.to_string()
+        sum.to_string()
     }
 
     fn part_2(&self, input: &str) -> String {
-        todo!()
+        let (root, dirs, files) = parse(input);
+        let remaining = 30_000_000 - (70_000_000 - size(root, &dirs, &files));
+        dirs.keys()
+            .filter_map(|con| {
+                let size = size(*con, &dirs, &files);
+                if size >= remaining {
+                    Some(size)
+                } else {
+                    None
+                }
+            })
+            .min()
+            .unwrap()
+            .to_string()
     }
 }
 
-fn size(dir: &str, dirs: &HashMap<&str, Vec<&str>>, files: &HashMap<&str, Vec<usize>>) -> usize {
-    files.get(dir).map(|v| v.iter().sum::<usize>()).unwrap_or(0)
+fn parse(input: &str) -> (u64, Dirs, Files) {
+    let mut dirs: Dirs = HashMap::new();
+    let mut files: Files = HashMap::new();
+    let mut dir_stack: Vec<&str> = vec!["/"];
+
+    let mut context: u64 = 0;
+    for line in input.lines() {
+        let mut line = line.split_whitespace();
+        match line.next().unwrap() {
+            // Command
+            "$" => match line.next().unwrap() {
+                "cd" => match line.next().unwrap() {
+                    "/" => dir_stack.truncate(1),
+                    ".." => {
+                        dir_stack.pop();
+                    }
+                    dir => dir_stack.push(dir),
+                },
+                "ls" => {
+                    let mut hasher = DefaultHasher::new();
+                    dir_stack.hash(&mut hasher);
+                    context = hasher.finish();
+                }
+                _ => unimplemented!(),
+            },
+            // ls output
+            item => match item {
+                "dir" => {
+                    let dir = line.next().unwrap();
+                    let mut hasher = DefaultHasher::new();
+                    let mut new = dir_stack.clone();
+                    new.push(dir);
+                    new.hash(&mut hasher);
+                    let hash = hasher.finish();
+                    dirs.entry(context).or_default().push(hash);
+                    dirs.insert(hash, Vec::new());
+                }
+                size => files
+                    .entry(context)
+                    .or_default()
+                    .push(size.parse().unwrap()),
+            },
+        }
+    }
+    let mut hasher = DefaultHasher::new();
+    vec!["/"].hash(&mut hasher);
+    let root = hasher.finish();
+    (root, dirs, files)
+}
+
+fn size(context: u64, dirs: &Dirs, files: &Files) -> u64 {
+    files
+        .get(&context)
+        .map(|v| v.iter().sum::<u64>())
+        .unwrap_or(0)
         + dirs
-            .get(dir)
+            .get(&context)
             .unwrap()
             .iter()
-            .map(|new_dir| size(new_dir, dirs, files))
-            .sum::<usize>()
+            .map(|new_context| size(*new_context, dirs, files))
+            .sum::<u64>()
 }
