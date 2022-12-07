4848ecbad53dfd0c70fcc0bebb43d5c9dc159d4e
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-06 22:46:13 -0600

    deduplicate

--- src/days/day_06.rs
+++ src/days/day_06.rs
@@ -6,38 +6,28 @@ pub struct Day06;
 
 impl Day for Day06 {
     fn part_1(&self, input: &str) -> String {
-        input
-            .chars()
-            .collect::<Vec<char>>()
-            .windows(4)
-            .enumerate()
-            .find(|(_i, window)| {
-                window
-                    .iter()
-                    .enumerate()
-                    .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
-            })
-            .unwrap()
-            .0
-            .add(4)
-            .to_string()
+        find::<4>(input)
     }
 
     fn part_2(&self, input: &str) -> String {
-        input
-            .chars()
-            .collect::<Vec<char>>()
-            .windows(14)
-            .enumerate()
-            .find(|(_i, window)| {
-                window
-                    .iter()
-                    .enumerate()
-                    .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
-            })
-            .unwrap()
-            .0
-            .add(14)
-            .to_string()
+        find::<14>(input)
     }
+}
+
+fn find<const W: usize>(input: &str) -> String {
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(W)
+        .enumerate()
+        .find(|(_i, window)| {
+            window
+                .iter()
+                .enumerate()
+                .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
+        })
+        .unwrap()
+        .0
+        .add(W)
+        .to_string()
 }
