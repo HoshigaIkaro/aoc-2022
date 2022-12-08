993da7f63081734d2e556534c5dc5d91cbc5b641
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-08 00:11:26 -0600

    changed to method chaining. benchmarks vary

--- src/days/day_08.rs
+++ src/days/day_08.rs
@@ -1,5 +1,3 @@
-use std::ops::{Add, Sub};
-
 use super::Day;
 
 pub struct Day08;
@@ -27,39 +25,43 @@ impl Day for Day08 {
 
     fn part_2(&self, input: &str) -> String {
         let grid = parse(input);
-        let mut score = 0;
         let rows = grid.len();
         let cols = grid[0].len();
-        for row in 1..rows - 1 {
-            for col in 1..cols - 1 {
-                let height = grid[row][col];
-                let left = (0..col)
-                    .enumerate()
-                    .rev()
-                    .find(|(_, c)| grid[row][*c] >= height)
-                    .map(|(index, _)| col - index)
-                    .unwrap_or(col);
-                let up = (0..row)
-                    .enumerate()
-                    .rev()
-                    .find(|(_, r)| grid[*r][col] >= height)
-                    .map(|(index, _)| row - index)
-                    .unwrap_or(row);
-                let right = (col + 1..cols)
-                    .enumerate()
-                    .find(|(_, c)| grid[row][*c] >= height)
-                    .map(|(index, _)| index + 1)
-                    .unwrap_or(cols - col - 1);
-                let down = (row + 1..rows)
-                    .enumerate()
-                    .find(|(_i, r)| grid[*r][col] >= height)
-                    .map(|(index, _)| index + 1)
-                    .unwrap_or(rows - row - 1);
-                let s = left * up * right * down;
-                score = score.max(s);
-            }
-        }
-        score.to_string()
+        (1..rows - 1)
+            .map(|row| {
+                (1..cols - 1)
+                    .map(|col| {
+                        let height = grid[row][col];
+                        let left = (0..col)
+                            .enumerate()
+                            .rev()
+                            .find(|(_, c)| grid[row][*c] >= height)
+                            .map(|(index, _)| col - index)
+                            .unwrap_or(col);
+                        let up = (0..row)
+                            .enumerate()
+                            .rev()
+                            .find(|(_, r)| grid[*r][col] >= height)
+                            .map(|(index, _)| row - index)
+                            .unwrap_or(row);
+                        let right = (col + 1..cols)
+                            .enumerate()
+                            .find(|(_, c)| grid[row][*c] >= height)
+                            .map(|(index, _)| index + 1)
+                            .unwrap_or(cols - col - 1);
+                        let down = (row + 1..rows)
+                            .enumerate()
+                            .find(|(_i, r)| grid[*r][col] >= height)
+                            .map(|(index, _)| index + 1)
+                            .unwrap_or(rows - row - 1);
+                        left * up * right * down
+                    })
+                    .max()
+                    .unwrap()
+            })
+            .max()
+            .unwrap()
+            .to_string()
     }
 }
 
