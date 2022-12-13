5bf44cd522920f7aba5f5cdeac865c7f64cab278
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-12 19:46:28 -0600

    optimize part 2

--- src/days/day_12.rs
+++ src/days/day_12.rs
@@ -59,47 +59,40 @@ impl Day for Day12 {
         let cols = input.find('\n').unwrap();
         let rows = chars.len() / cols;
 
-        let end_index = chars.iter().position(|c| *c == 'E').unwrap();
-        let end_point = (end_index % cols, end_index / cols);
+        let start_index = chars.iter().position(|c| *c == 'E').unwrap();
+        let start_point = (start_index % cols, start_index / cols);
 
-        let board: Vec<usize> = chars.into_iter().map(char_to_height).collect();
+        let board: Vec<usize> = chars.into_iter().map(char_to_height_reverse).collect();
 
-        board
-            .iter()
-            .enumerate()
-            .filter_map(|(i, h)| if *h == 0 { Some(i) } else { None })
-            .map(|start_index| {
-                let start_point = (start_index % cols, start_index / cols);
-                let mut visited: HashSet<Point> = HashSet::new();
-                let mut queue: VecDeque<Point> = std::iter::once(start_point).collect();
-                let mut dist: HashMap<Point, usize> = std::iter::once((start_point, 0)).collect();
+        let mut visited: HashSet<Point> = HashSet::new();
+        let mut queue: VecDeque<Point> = std::iter::once(start_point).collect();
+        let mut dist: HashMap<Point, usize> = std::iter::once((start_point, 0)).collect();
 
-                while let Some(point @ (x, y)) = queue.pop_front() {
-                    if !visited.insert(point) {
-                        continue;
+        while let Some(point @ (x, y)) = queue.pop_front() {
+            if !visited.insert(point) {
+                continue;
+            }
+
+            let current_height = board[y * cols + x];
+            for new_point @ (x_n, y_n) in get_new_positions(point, rows, cols) {
+                let new_height = board[y_n * cols + x_n];
+                if new_height == 25 {
+                    if current_height == 24 {
+                        let steps = dist[&point] + 1;
+                        return steps.to_string();
                     }
-
-                    if point == end_point {
-                        break;
-                    }
-
-                    let current_height = board[y * cols + x];
-                    for new_point @ (x_n, y_n) in get_new_positions(point, rows, cols) {
-                        let new_height = board[y_n * cols + x_n];
-                        if new_height <= current_height + 1 {
-                            let current_dist = dist[&point];
-
-                            dist.insert(new_point, current_dist + 1);
-                            queue.push_back(new_point);
-                        }
-                    }
+                    continue;
                 }
-                dist.get(&end_point).copied()
-            })
-            .filter_map(|c| c)
-            .min()
-            .unwrap()
-            .to_string()
+                if new_height <= current_height + 1 {
+                    let current_dist = dist[&point];
+
+                    dist.insert(new_point, current_dist + 1);
+                    queue.push_back(new_point);
+                }
+            }
+        }
+
+        unreachable!()
     }
 }
 
@@ -108,6 +101,15 @@ fn char_to_height(c: char) -> usize {
         'a'..='z' => c as usize - 'a' as usize,
         'S' => 0,
         'E' => 25,
+        _ => unreachable!(),
+    }
+}
+
+fn char_to_height_reverse(c: char) -> usize {
+    match c {
+        'a'..='z' => 'z' as usize - c as usize,
+        'S' => 25,
+        'E' => 0,
         _ => unreachable!(),
     }
 }
