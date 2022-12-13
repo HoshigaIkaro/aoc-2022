284ed16b777a791d02cd712427c0bfdaa67d739a
HoshigaIkaro <hoshiga.ikaro@outlook.com>
2022-12-13 13:41:32 -0600

    add extra timing information and color

--- examples/all.rs
+++ examples/all.rs
@@ -1,12 +1,30 @@
-use std::time::Instant;
+use owo_colors::OwoColorize;
+use std::time::{Duration, Instant};
 
 use aoc::run_day;
 
 fn main() {
-    let start = Instant::now();
-    for day in 1..=12 {
+    let mut timings = Vec::new();
+    for day in 1..=13 {
+        let start = Instant::now();
         run_day(day);
+        timings.push(start.elapsed());
     }
-    let time = start.elapsed();
-    println!("{} µs or {} ms", time.as_micros(), time.as_millis());
+    let mut total = Duration::default();
+    for (day, time) in timings.into_iter().enumerate() {
+        total += time;
+        println!(
+            "{}",
+            format!("Day {: >2} : {: >5} µs", day + 1, time.as_micros()).fg_rgb::<186, 187, 241>()
+        );
+    }
+    println!();
+    println!(
+        "{}",
+        format!(
+            "Total time: {} µs or {} ms",
+            total.as_micros(),
+            total.as_millis()
+        ).fg_rgb::<140, 170, 238>()
+    );
 }
