d75b968f49ebb2ccac5df4037ffdaaf5e38cdc64
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-14 00:38:45 -0600

    add day 14 to example all

--- examples/all.rs
+++ examples/all.rs
@@ -5,7 +5,7 @@ use aoc::run_day;
 
 fn main() {
     let mut timings = Vec::new();
-    for day in 1..=13 {
+    for day in 1..=14 {
         let start = Instant::now();
         run_day(day);
         timings.push(start.elapsed());
