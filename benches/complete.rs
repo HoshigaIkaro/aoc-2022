401367751cfef4081a8e4a99ff0732ec09c3b397
HoshigaIkaro <hoshiga.ikaro@outlook.com>
2022-12-16 14:53:09 -0600

    successfully computes the example input

--- benches/complete.rs
+++ benches/complete.rs
@@ -49,9 +49,10 @@ bench_day!(12);
 bench_day!(13);
 bench_day!(14);
 bench_day!(15);
+bench_day!(16);
 
 criterion_group!(
     complete, day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10,
-    day_11, day_12, day_13, day_14, day_15
+    day_11, day_12, day_13, day_14, day_15, day_16,
 );
 criterion_main!(complete);
--- src/days/day_16.rs
+++ src/days/day_16.rs
@@ -1,6 +1,6 @@
 use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap, HashSet},
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    hash::Hash,
 };
 
 use super::Day;
@@ -9,67 +9,23 @@ use super::Day;
 struct Valve<'a> {
     flow_rate: usize,
     connected: Vec<&'a str>,
+    open: bool,
 }
 
-fn traverse<'a>(
-    valves: &HashMap<&str, Valve>,
-    mut opened: HashSet<&'a str>,
-    current: &'a str,
-    mut total_flow: usize,
-    mut pressure: usize,
-    mut minute: usize,
-) -> usize {
-    minute += 1;
-    pressure += total_flow;
-    if minute == 30 {
-        println!("{opened:?}");
-        return pressure;
-    }
-
-    let current_valve = valves.get(current).unwrap();
-    if current_valve.flow_rate == 0 || opened.contains(&current) {
-        let mut possible: Vec<&str> = valves[&current]
-            .connected
-            .iter()
-            .map(|v| *v)
-            .collect();
-        possible.sort_by_key(|v| Reverse(valves[v].flow_rate));
-        // println!("{possible:?}");
-        let mut m = 0;
-        for valve in possible {
-            m = m.max(traverse(valves, opened.clone(), valve, total_flow, pressure, minute));
-        }
-        m
-    } else {
-        let mut possible: Vec<&str> = valves[&current]
-            .connected
-            .iter()
-            .filter(|v| !opened.contains(**v))
-            .map(|v| *v)
-            .collect();
-        possible.sort_by_key(|v| Reverse(valves[v].flow_rate));
-        if let Some(&better) = possible
-            .iter()
-            .find(|v| valves.get(*v).unwrap().flow_rate > current_valve.flow_rate)
-        {
-            traverse(valves, opened, better, total_flow, pressure, minute)
-        } else {
-            opened.insert(current);
-            total_flow += current_valve.flow_rate;
-            traverse(valves, opened, current, total_flow, pressure, minute)
-        }
-    }
+#[derive(Debug)]
+struct Connection {
+    flow_rate: usize,
+    distance: usize,
 }
-
+type ReducedMap<'a> = HashMap<&'a str, HashMap<&'a str, Connection>>;
 pub struct Day16;
 
 impl Day for Day16 {
     fn part_1(&self, input: &str) -> String {
         let valves = parse_valves(input);
-        let opened = HashSet::new();
-        let m = traverse(&valves, opened, "AA", 0, 0, 0);
-        dbg!(m);
-        todo!()
+        let basic_map = get_reduced_map(&valves);
+        // dbg!(&basic_map["AA"]);
+        traverse(basic_map).to_string()
     }
 
     fn part_2(&self, input: &str) -> String {
@@ -93,10 +49,202 @@ fn parse_valves(input: &str) -> HashMap<&str, Valve> {
         let valve = Valve {
             flow_rate,
             connected,
+            open: false,
         };
         valves.insert(valve_name, valve);
     }
     valves
+}
+
+/// Calculates the distance from each non-zero valve to another.
+///
+/// Valves with a flow rate of 0 will not be included in the calculations,
+/// unless it is "AA", the starting position.
+fn get_reduced_map<'a>(valves: &HashMap<&'a str, Valve<'a>>) -> ReducedMap<'a> {
+    let mut full: HashMap<&str, HashMap<&str, Connection>> = HashMap::with_capacity(valves.len());
+    for source in valves.keys() {
+        // skips non-zero roots unless it is the start
+        if valves[source].flow_rate == 0 && *source != "AA" {
+            continue;
+        }
+        // calculate the distance for each target
+        for target in valves.keys() {
+            // except for targets with zero flow rate, the start, and the same valve as source
+            if valves[target].flow_rate == 0 || target == source || *target == "AA" {
+                continue;
+            }
+            full.entry(source)
+                .or_default()
+                .insert(target, find_connection(source, target, &valves));
+        }
+    }
+    full
+}
+
+/// Finds the connection from `source` to `target` in the `values` map.
+///
+/// The current implementation searches breadth first with a `VecDeque`.
+fn find_connection<'a>(
+    source: &'a str,
+    target: &'a str,
+    valves: &HashMap<&'a str, Valve<'a>>,
+) -> Connection {
+    let mut visited: HashSet<&str> = HashSet::new();
+    let mut distance: HashMap<&str, usize> = std::iter::once((source, 0)).collect();
+    let mut queue: VecDeque<&str> = std::iter::once(source).collect();
+    while let Some(current) = queue.pop_front() {
+        if current == target {
+            break;
+        }
+
+        for possible in &valves[current].connected {
+            if !visited.insert(possible) {
+                continue; // already visited
+            }
+            distance.insert(possible, distance[&current] + 1);
+            queue.push_back(possible);
+        }
+    }
+    Connection {
+        flow_rate: valves[&target].flow_rate,
+        distance: distance[&target],
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Default)]
+struct State<'a> {
+    current_valve: &'a str,
+    elapsed_minutes: usize,
+    pressure: usize,
+    flow_rate: usize,
+    opened_valves: Vec<&'a str>,
+}
+
+impl<'a> State<'a> {
+    fn new(current: &'a str) -> Self {
+        Self {
+            current_valve: current,
+            elapsed_minutes: 0,
+            pressure: 0,
+            flow_rate: 0,
+            opened_valves: Vec::new(),
+        }
+    }
+}
+
+impl<'a> PartialOrd for State<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for State<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let left = self.pressure + self.flow_rate * (30 - self.elapsed_minutes);
+        let right = other.pressure + other.flow_rate * (30 - other.elapsed_minutes);
+        left.cmp(&right)
+    }
+}
+
+/// Finds the path with the most pressure after 30 minutes and returns the value.
+///
+/// Works only on the example input.
+fn traverse<'a>(valves: ReducedMap<'a>) -> usize {
+    let max_valves = valves.len() - 1; // remove count for "AA"
+    let mut queue: BinaryHeap<State> = BinaryHeap::new();
+    queue.push(State::new("AA"));
+
+    let mut best = 0;
+    let mut best_state = State::default();
+    while let Some(state) = queue.pop() {
+        // movement unavailable
+        if state.opened_valves.len() == max_valves || state.elapsed_minutes == 30 {
+            let pressure = state.pressure + state.flow_rate * (30 - state.elapsed_minutes);
+            if pressure > best {
+                best = pressure;
+                best_state = state;
+            }
+            continue;
+        }
+
+        for &possible in valves.keys().filter(|new| {
+            !state.opened_valves.contains(*new) && **new != "AA" && **new != state.current_valve
+        }) {
+            let mut state = state.clone();
+            let connection = &valves[state.current_valve][possible];
+            if state.elapsed_minutes + connection.distance > 30 {
+                continue;
+            }
+            // move to the new valve
+            state.current_valve = possible;
+            state.elapsed_minutes += connection.distance;
+            state.pressure += state.flow_rate * connection.distance;
+            if state.elapsed_minutes == 30 {
+                queue.push(state);
+                continue;
+            }
+            // turn the valve
+            state.elapsed_minutes += 1;
+            state.pressure += state.flow_rate;
+            state.opened_valves.push(possible);
+            state.flow_rate += connection.flow_rate;
+
+            queue.push(state);
+        }
+    }
+    dbg!(&best_state);
+    best_state.pressure + best_state.flow_rate * (30 - best_state.elapsed_minutes)
+}
+
+fn traverse_alpha<'a>(
+    valves: &mut HashMap<&'a str, Valve<'a>>,
+    full: &HashMap<&'a str, HashMap<&str, Connection>>,
+    mut open: HashSet<&'a str>,
+    current: &'a str,
+    mut pressure: usize,
+    mut total_flow: usize,
+    mut minutes: usize,
+) -> usize {
+    // println!("{}", current);
+    pressure += total_flow;
+    if open.len() == 0 {
+        // dbg!(minutes);
+        let remaining_minutes = 30 - minutes;
+        pressure + total_flow * remaining_minutes.saturating_sub(1)
+    } else {
+        let max_distance = open
+            .iter()
+            .filter(|c| full[current][*c].distance + minutes <= 30)
+            .map(|op| full[current][op].distance)
+            .max()
+            .unwrap();
+        let best = open
+            .iter()
+            .max_by_key(|c| {
+                let conn = &full[current][*c];
+                let mut pressure = 0;
+                // move to place
+                pressure += total_flow * conn.distance;
+                // turn the valve
+                pressure += total_flow;
+                // finish waiting
+                pressure += (total_flow + conn.flow_rate) * (max_distance + 1 - conn.distance + 1);
+                pressure
+            })
+            .unwrap();
+        dbg!(best);
+        let conn = &full[current][*best];
+        if conn.distance + minutes >= 30 {
+            dbg!('a');
+        }
+        minutes += conn.distance + 1;
+        pressure += total_flow * conn.distance;
+        total_flow += conn.flow_rate;
+        let mut open = open.clone();
+        open.remove(best);
+
+        traverse_alpha(valves, full, open, best, pressure, total_flow, minutes)
+    }
 }
 
 #[cfg(test)]
@@ -111,7 +259,8 @@ mod day_16_tests {
             valves["AA"],
             Valve {
                 flow_rate: 0,
-                connected: vec!["DD", "II", "BB"]
+                connected: vec!["DD", "II", "BB"],
+                open: false
             }
         )
     }
