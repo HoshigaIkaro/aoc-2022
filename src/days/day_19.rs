dc00a4b47da71067aa291a00282298a082e0e64f
HoshigaIkaro <hoshiga.ikaro@outlook.com>
2022-12-19 15:17:26 -0600

    works for the first example

--- src/days/day_19.rs
+++ src/days/day_19.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashSet, VecDeque},
-    sync::atomic::{AtomicUsize, Ordering}, io::Write,
+    io::Write,
+    sync::atomic::{AtomicUsize, Ordering},
 };
 
 use rayon::prelude::*;
@@ -55,10 +56,10 @@ struct Pack {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Action {
-    Ore(usize),
-    Clay(usize),
-    Obsidian(usize),
-    Geode(usize),
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
     Wait,
 }
 
@@ -70,6 +71,7 @@ struct Blueprint {
     pack: Pack,
     minutes: usize,
     action: Action,
+    previous: Action,
 }
 
 impl Blueprint {
@@ -87,6 +89,7 @@ impl Blueprint {
             pack: Pack::default(),
             minutes: 0,
             action: Action::Wait,
+            previous: Action::Wait,
         }
     }
 
@@ -104,14 +107,17 @@ impl Blueprint {
             let mut state = self.clone();
             state.pack.ore -= self.costs.geode.0;
             state.pack.obsidian -= self.costs.geode.1;
-            state.action = Action::Geode(1);
+            state.action = Action::Geode;
             states.push(state);
         }
-        if self.pack.ore >= self.costs.obsidian.0 && self.pack.clay >= self.costs.obsidian.1 {
+        if self.pack.ore >= self.costs.obsidian.0
+            && self.pack.clay >= self.costs.obsidian.1
+            && (self.rates.obsidian / self.rates.ore < self.costs.geode.1 / self.costs.geode.0)
+        {
             let mut state = self.clone();
             state.pack.ore -= self.costs.obsidian.0;
             state.pack.clay -= self.costs.obsidian.1;
-            state.action = Action::Obsidian(1);
+            state.action = Action::Obsidian;
             states.push(state);
         }
         if self.pack.ore >= self.costs.clay
@@ -119,27 +125,28 @@ impl Blueprint {
         {
             let mut state = self.clone();
             state.pack.ore -= self.costs.clay;
-            state.action = Action::Clay(1);
+            state.action = Action::Clay;
             states.push(state);
         }
-        if self.pack.ore >= 4 {
-            let mut state = self.clone();
-            state.pack.ore -= self.costs.ore;
-            state.action = Action::Ore(1);
-            states.push(state);
-        }
+        // if self.pack.ore > 3 && self.rates.ore < 4 {
+        //     let mut state = self.clone();
+        //     state.pack.ore -= self.costs.ore;
+        //     state.action = Action::Ore(1);
+        //     states.push(state);
+        // }
 
         states
             .iter_mut()
             .map(|state| {
                 state.add();
                 match state.action {
-                    Action::Ore(_) => state.rates.ore += 1,
-                    Action::Clay(_) => state.rates.clay += 1,
-                    Action::Obsidian(_) => state.rates.obsidian += 1,
-                    Action::Geode(_) => state.rates.geode += 1,
+                    Action::Ore => state.rates.ore += 1,
+                    Action::Clay => state.rates.clay += 1,
+                    Action::Obsidian => state.rates.obsidian += 1,
+                    Action::Geode => state.rates.geode += 1,
                     Action::Wait => (),
                 }
+                state.previous = state.action;
                 state.action = Action::Wait;
                 state.minutes += 1;
                 *state
@@ -148,11 +155,13 @@ impl Blueprint {
     }
 
     fn score(&self) -> usize {
-        (self.rates.ore + self.rates.clay * 4 + self.rates.obsidian * 32 + self.rates.geode * 64)
+        // (self.rates.ore + self.rates.clay * 4 +
+        self.rates.obsidian * 32 + self.rates.geode * 64
+            // )
             + (24 - self.minutes)
                 * (self.rates.ore
                     + self.rates.clay * 4
-                    + self.rates.obsidian * 16
+                    + self.rates.obsidian * 32
                     + self.rates.geode * 64)
     }
 }
@@ -162,44 +171,49 @@ pub struct Day19;
 impl Day for Day19 {
     fn part_1(&self, input: &str) -> String {
         let bl = Blueprint::new(1, 4, 2, (3, 14), (2, 7));
+        // let bl = Blueprint::new(2, 2, 3, (3, 8), (3, 12));
         let mut queue = VecDeque::new();
         queue.push_back(bl);
         let mut max_geode = 0;
-        let mut best = [0; 24];
+        let best = AtomicUsize::new(0);
         let mut i = 0;
         let mut log_file = std::fs::File::create("log.log").unwrap();
         loop {
             dbg!(i);
+            dbg!(queue.len());
             if queue.is_empty() {
                 break;
             }
-            let mut sub = Vec::new();
-            for state in queue.drain(0..) {
-                if state.score() < best[state.minutes] {
-                    continue;
-                }
-                best[state.minutes] = best[state.minutes].max(state.score());
-                sub.push(state);
-            }
-            let new: Vec<Blueprint> = sub
+            let mut reduced = queue
+                .par_drain(0..)
+                .filter_map(|state| {
+                    if state.pack.geode >= best.load(Ordering::Relaxed) {
+                        best.fetch_max(state.pack.geode, Ordering::Release);
+                        Some(state)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+            let new: Vec<Blueprint> = reduced
                 .par_iter_mut()
                 .flat_map(|state| {
-                    if state.minutes == 24 {
-                        vec![*state]
-                    } else {
-                        state.advance()
-                    }
+                    // if state.minutes == 24 {
+                    //     vec![*state]
+                    // } else {
+                    state.advance()
+                    // }
                 })
                 .collect();
             for state in new {
                 max_geode = state.pack.geode.max(max_geode);
-                // if state.rates.geode == 1 {
-                //     dbg!(state.score());
-                // }
+                // println!("{:?}", state);
+                if state.rates.geode == 2 {
+                    // break 'outer;
+                }
+                writeln!(log_file, "{:?}", state).unwrap();
                 if state.minutes < 24 {
                     queue.push_back(state);
-                } else {
-                    writeln!(log_file, "{:?}", state).unwrap();
                 }
             }
             i += 1;
