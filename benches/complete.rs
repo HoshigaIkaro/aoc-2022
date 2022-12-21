7988c2ac22dd2f0971f3d2841af4905d37155e3b
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-21 00:04:06 -0600

    add benchmark

--- benches/complete.rs
+++ benches/complete.rs
@@ -68,9 +68,10 @@ bench_day!(17);
 bench_day!(18);
 bench_day!(19);
 bench_day!(20);
+bench_day!(21);
 
 criterion_group!(
     complete, day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10,
-    day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20
+    day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20, day_21
 );
 criterion_main!(complete);
