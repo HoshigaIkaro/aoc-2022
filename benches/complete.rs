49cc4d48a931aa5cf60c9aea34c0b4efba5db0d7
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-23 01:18:36 -0600

    create benchmarks for days 22 and 23

--- benches/complete.rs
+++ benches/complete.rs
@@ -38,13 +38,9 @@ fn day_16(c: &mut Criterion) {
     let input = load_input(16);
     let mut group = c.benchmark_group("day_16");
     let day = get_day!(16);
-    group.bench_with_input("part_1", &input, |b, input| {
-        b.iter(|| day.part_1(&input))
-    });
+    group.bench_with_input("part_1", &input, |b, input| b.iter(|| day.part_1(&input)));
     group.sample_size(10);
-    group.bench_with_input("part_2", &input, |b, input| {
-        b.iter(|| day.part_2(&input))
-    });
+    group.bench_with_input("part_2", &input, |b, input| b.iter(|| day.part_2(&input)));
     group.finish();
 }
 
@@ -69,9 +65,12 @@ bench_day!(18);
 bench_day!(19);
 bench_day!(20);
 bench_day!(21);
+bench_day!(22);
+bench_day!(23);
 
 criterion_group!(
     complete, day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10,
-    day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20, day_21
+    day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20, day_21, day_22,
+    day_23
 );
 criterion_main!(complete);
