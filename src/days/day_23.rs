e9c1680e51f6a7ec958f06c25f1380ed6ad08c36
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-23 10:29:38 -0600

    change elf id system

--- src/days/day_23.rs
+++ src/days/day_23.rs
@@ -4,8 +4,6 @@ use rustc_hash::FxHashMap;
 
 use super::Day;
 
-type Id = usize;
-
 // Point(x, y)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point(isize, isize);
@@ -47,15 +45,13 @@ impl Direction {
 }
 
 struct Elf {
-    id: usize,
     position: Point,
     directions: [Direction; 4],
 }
 
 impl Elf {
-    fn new(id: Id, position: Point) -> Self {
+    fn new(position: Point) -> Self {
         Self {
-            id,
             position,
             directions: [
                 Direction::North,
@@ -79,29 +75,29 @@ impl Elf {
         ]
     }
 
-    fn any_elves_around(&self, elves: &FxHashMap<Id, Elf>) -> bool {
+    fn any_elves_around(&self, elves: &[Elf]) -> bool {
         self.deltas_around().into_iter().any(|delta| {
             let new = self.position + delta;
-            elves.values().any(|elf| elf.position == new)
+            elves.iter().any(|elf| elf.position == new)
         })
     }
 
-    fn propose(&self, elves: &FxHashMap<Id, Elf>, proposed: &mut FxHashMap<Point, Vec<Id>>, start_index:usize) {
+    fn propose(&self, elves: &[Elf], start_index: usize) -> Option<Point> {
         if self.any_elves_around(elves) {
             for index in 0..4 {
                 let new_index = (start_index + index) % 4;
                 let direction = self.directions[new_index];
                 if direction.deltas_to_check().into_iter().all(|delta| {
                     let new = self.position + delta;
-                    elves.values().all(|elf| elf.position != new)
+                    elves.iter().all(|elf| elf.position != new)
                 }) {
                     // can propose
                     let new = direction.new_point(self.position);
-                    proposed.entry(new).or_default().push(self.id);
-                    break;
+                    return Some(new);
                 }
             }
         }
+        None
     }
 }
 
@@ -112,23 +108,25 @@ impl Day for Day23 {
         let mut elves = parse_elves(input);
         for round in 0..10 {
             // first half
-            let mut proposed = FxHashMap::default();
-            for elf in elves.values() {
-                elf.propose(&elves, &mut proposed, round % 4);
+            let mut proposed: FxHashMap<Point, Vec<usize>> = FxHashMap::default();
+            for (id, elf) in elves.iter().enumerate() {
+                if let Some(new_point) = elf.propose(&elves, round % 4) {
+                    proposed.entry(new_point).or_default().push(id);
+                }
             }
 
             // second half
             for (point, elf_ids) in proposed {
                 if let [elf_id] = elf_ids.as_slice() {
-                    elves.get_mut(elf_id).unwrap().position = point
+                    elves.get_mut(*elf_id).unwrap().position = point
                 }
             }
         }
         // display_elves(&elves);
-        let min_x = elves.values().map(|elf| elf.position.0).min().unwrap();
-        let max_x = elves.values().map(|elf| elf.position.0).max().unwrap();
-        let min_y = elves.values().map(|elf| elf.position.1).min().unwrap();
-        let max_y = elves.values().map(|elf| elf.position.1).max().unwrap();
+        let min_x = elves.iter().map(|elf| elf.position.0).min().unwrap();
+        let max_x = elves.iter().map(|elf| elf.position.0).max().unwrap();
+        let min_y = elves.iter().map(|elf| elf.position.1).min().unwrap();
+        let max_y = elves.iter().map(|elf| elf.position.1).max().unwrap();
 
         let width = min_x.abs_diff(max_x) + 1;
         let length = min_y.abs_diff(max_y) + 1;
@@ -141,38 +139,38 @@ impl Day for Day23 {
     fn part_2(&self, input: &str) -> String {
         let mut elves = parse_elves(input);
         let mut round = 1;
-        let first = loop {
+        loop {
             // first half
-            let mut proposed = FxHashMap::default();
-            for elf in elves.values() {
-                elf.propose(&elves, &mut proposed,round % 4);
+            let mut proposed: FxHashMap<Point, Vec<usize>> = FxHashMap::default();
+            for (id, elf) in elves.iter().enumerate() {
+                if let Some(new_point) = elf.propose(&elves, round % 4) {
+                    proposed.entry(new_point).or_default().push(id);
+                }
             }
 
             if proposed.is_empty() {
-                break round;
+                break;
             }
 
             // second half
             for (point, elf_ids) in proposed {
                 if let [elf_id] = elf_ids.as_slice() {
-                    elves.get_mut(elf_id).unwrap().position = point
+                    elves.get_mut(*elf_id).unwrap().position = point
                 }
             }
             round += 1;
         };
-        first.to_string()
+        round.to_string()
     }
 }
 
-fn parse_elves(input: &str) -> FxHashMap<Id, Elf> {
-    let mut elves = FxHashMap::default();
-    let mut id = 0;
+fn parse_elves(input: &str) -> Vec<Elf> {
+    let mut elves = Vec::new();
     for (y, row) in input.lines().enumerate() {
         for (x, tile) in row.chars().enumerate() {
             if tile == '#' {
-                let elf = Elf::new(id, Point(x as isize, y as isize));
-                elves.insert(id, elf);
-                id += 1;
+                let elf = Elf::new(Point(x as isize, y as isize));
+                elves.push(elf)
             }
         }
     }
