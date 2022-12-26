4d3a51b27fc426cdf5976b1e4f432c883528ca83
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2022-12-25 18:13:03 -0600

    simplify literal expressions

--- src/days/day_02.rs
+++ src/days/day_02.rs
@@ -9,17 +9,17 @@ impl Day for Day02 {
             .lines()
             .map(|s| match s {
                 // Opponent: Rock
-                "A X" => 1 + 3,
-                "A Y" => 2 + 6,
-                "A Z" => 3 + 0,
+                "A X" => 4, // 1 + 3
+                "A Y" => 8, // 2 + 6
+                "A Z" => 3, // 3 + 0
                 // Opponent: Paper
-                "B X" => 1 + 0,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
+                "B X" => 1, // 1 + 0
+                "B Y" => 5, // 2 + 3
+                "B Z" => 9, // 3 + 6
                 // Opponent: Scissors
-                "C X" => 1 + 6,
-                "C Y" => 2 + 0,
-                "C Z" => 3 + 3,
+                "C X" => 7, // 1 + 6
+                "C Y" => 2, // 2 + 0
+                "C Z" => 6, // 3 + 3
                 _ => unreachable!(),
             })
             .sum::<usize>()
@@ -31,17 +31,17 @@ impl Day for Day02 {
             .lines()
             .map(|s| match s {
                 // Opponent: Rock
-                "A X" => 3 + 0,
-                "A Y" => 1 + 3,
-                "A Z" => 2 + 6,
+                "A X" => 3, // 3 + 0
+                "A Y" => 4, // 1 + 3
+                "A Z" => 8, // 2 + 6
                 // Opponent: Paper
-                "B X" => 1 + 0,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
+                "B X" => 1, // 1 + 0
+                "B Y" => 5, // 2 + 3
+                "B Z" => 9, // 3 + 6
                 // Opponent: Scissors
-                "C X" => 2 + 0,
-                "C Y" => 3 + 3,
-                "C Z" => 1 + 6,
+                "C X" => 2, // 2 + 0
+                "C Y" => 6, // 3 + 3
+                "C Z" => 7, // 1 + 6
                 _ => unreachable!(),
             })
             .sum::<usize>()
